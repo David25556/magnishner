4a898e770129246c27fd9c0f308975ee9924273f
David25556 <david.xaratyan@tumo.org>
2023-07-11 18:32:58 +0400

    verjnakan

--- countris.js
+++ countris.js
@@ -1,66 +1,66 @@
 const countries = [
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/descarga%20(1).png",
+        "magnish": "./magnish-20230701T131232Z-001/descarga%20(1).png",
         "name": "Միցուբիշի"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/descarga%20(2).png",
+        "magnish": "./magnish-20230701T131232Z-001/descarga%20(2).png",
         "name": "Սուզուկի"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/descarga%20(3).png",
+        "magnish": "./magnish-20230701T131232Z-001/descarga%20(3).png",
         "name": "Օպել"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/descarga%20(4).png",
+        "magnish": "./magnish-20230701T131232Z-001/descarga%20(4).png",
         "name": "Սկոդա"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/descarga%20(5).png",
+        "magnish": "./magnish-20230701T131232Z-001/download.jpeg",
         "name": "Հինո"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/descarga.png",
+        "magnish": "./magnish-20230701T131232Z-001/descarga.png",
         "name": "Ռոլս Ռոիս"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/desktop-wallpaper-hyundai-carbon-logo-grunge-art-carbon-background-creative-hyundai-black-logo-cars-brands-hyundai-logo-hyundai-thumbnail.jpg",
+        "magnish": "./magnish-20230701T131232Z-001/desktop-wallpaper-hyundai-carbon-logo-grunge-art-carbon-background-creative-hyundai-black-logo-cars-brands-hyundai-logo-hyundai-thumbnail.jpg",
         "name": "Հյոնդա"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/desktop-wallpaper-lexus-carbon-lexus-logo-thumbnail.jpg",
+        "magnish": "./magnish-20230701T131232Z-001/desktop-wallpaper-lexus-carbon-lexus-logo-thumbnail.jpg",
         "name": "Լեքսուս"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(1).png",
+        "magnish": "./magnish-20230701T131232Z-001/download%20(1).png",
         "name": "Բենտլի"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(2).png",
+        "magnish": "./magnish-20230701T131232Z-001/download%20(2).png",
         "name": "Տեսլա"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download.jpeg",
+        "magnish": "./magnish-20230701T131232Z-001/download.jpeg",
         "name": "Յագուառ"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(3).png",
+        "magnish": "./magnish-20230701T131232Z-001/download%20(3).png",
         "name": "Աուդի"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(4).png",
+        "magnish": "./magnish-20230701T131232Z-001/download%20(4).png",
         "name": "Բուգածի"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(5).png",
+        "magnish": "./magnish-20230701T131232Z-001/download%20(5).png",
         "name": "Տոյոտա"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(6).png",
+        "magnish": "./magnish-20230701T131232Z-001/images.jpeg",
         "name": "Լինկոլն"
     },
     {
-        "magnish": "file:///Users/user/Desktop/dav%201/magnish-20230701T131232Z-001/download%20(3).jpeg",
+        "magnish": "./magnish-20230701T131232Z-001/download%20(3).jpeg",
         "name": "Սեատ"
     },
 ]
